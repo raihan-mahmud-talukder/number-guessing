97d23d5ad0350c2f4a6ee5119fa8e9fab79f8fae
Raihan Mahmud <raihan_uits@outlook.com>
2023-03-31 01:41:52 +0600

    injecting to DOM

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
-const secretNumber = Math.trunc(Math.random() * 10 + 1)
-let guess = 3
-let loop = false
+const check = document.getElementsByTagName('section')[0].getElementsByTagName('button')[0]
+const display = document.getElementsByTagName('section')[0].getElementsByTagName('p')[0]
+const input = document.getElementsByTagName('section')[0].getElementsByTagName('input')[0]
+const again = document.getElementsByTagName('header')[0].getElementsByTagName('button')[0]
+const number = document.getElementsByTagName('header')[0].getElementsByTagName('div')[0]
+const chance = document.getElementsByTagName('aside')[0]
 
-while (!loop) {
-    console.log(`You have ${guess} guesses left`)
-    let guessed_num = Number(prompt('Guess a number:'))
-    if (!isNaN(guessed_num)) {
+let secretNumber = Math.trunc(Math.random() * 10 + 1)
+let guess = 3
+
+const displayMessage = message => {
+    display.innerHTML = message
+}
+
+const chanceMessage = message => {
+    chance.innerHTML = message
+}
+chanceMessage(`You have ${guess} guesses left`)
+
+check.addEventListener('click', () => {
+    const guessed_num = input.value
+    if (guessed_num) {
         if (guessed_num > secretNumber) {
             guess--
-            console.log(`higher`)
+            displayMessage('greater')
+            chanceMessage(`You have ${guess} guesses left`)
         } else if (guessed_num < secretNumber) {
             guess--
-            console.log(`Lower`)
+            displayMessage('smaller')
+            chanceMessage(`You have ${guess} guesses left`)
         } else {
-            console.log(`Correct Number`)
-            loop = true
+            document.querySelector('body').style.background = 'green'
+            number.innerHTML = guessed_num
+            input.disabled = true
+            check.disabled = true
+            displayMessage(`Correct Number`)
+            chanceMessage(`You Won!`)
         }
         if (guess == 0) {
-            console.log(`You Loose`)
-            loop = true
-            console.log(`Number to be guessed:`, secretNumber)
+            input.disabled = true
+            check.disabled = true
+            displayMessage(`Number to be guessed: ${secretNumber}`)
+            chanceMessage(`You Lost!`)
         }
     } else {
-        loop = true
-        console.log(`Invalid Input!`)
+        displayMessage(`Invalid Input!`)
     }
-}
+})
+
+again.addEventListener('click', () => {
+    secretNumber = Math.trunc(Math.random() * 10 + 1)
+    guess = 3
+    input.value = ''
+    check.disabled = false
+    input.disabled = false
+    document.querySelector('body').style.background = 'black'
+    number.innerHTML = '?'
+    displayMessage('Start Guessing...')
+    chanceMessage(`You have ${guess} guesses left`)
+})
